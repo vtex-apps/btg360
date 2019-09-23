6fcf3fd85f669b06914552d1dce801875979184a
Julio Moreira <hellojuliomoreira@gmail.com>
2019-09-23 11:47:15 -0300

    Fixes linting.

--- react/index.tsx
+++ react/index.tsx
@@ -1,51 +1,59 @@
-import { PixelMessage } from "./typings/events";
-import { canUseDOM } from "vtex.render-runtime";
+import { PixelMessage } from './typings/events'
+import { canUseDOM } from 'vtex.render-runtime'
 
 function handleMessages(e: PixelMessage) {
   const {
     __btg360: { BTGId: account, BTGDomain: domain },
-    location: { pathname }
-  } = window;
+    location: { pathname },
+  } = window
   switch (e.data.eventName) {
-    case "vtex:pageInfo": {
-      if (e.data.eventType === "internalSiteSearchView") {
+    case 'vtex:pageInfo': {
+      if (e.data.eventType === 'internalSiteSearchView') {
         const items = pathname
-          .split("?")[0]
-          .split("/")
+          .split('?')[0]
+          .split('/')
           .filter(item => item.toLocaleLowerCase())
-          .map(keyword => ({ keyword }));
-        const event = "search";
+          .map(keyword => ({ keyword }))
+        const event = 'search'
         const BTG360SearchEvent = {
           account,
           domain,
           event,
-          items
-        };
-        Btg360.add(BTG360SearchEvent);
+          items,
+        }
+        Btg360.add(BTG360SearchEvent)
       }
-      break;
+      break
     }
-    case "vtex:orderPlaced": {
-      const { transactionId, transactionProducts: items } = e.data;
+    case 'vtex:orderPlaced': {
+      const { transactionId, transactionProducts: items } = e.data
       const BTG360TransactionEvent = {
         account,
         domain,
-        event: "transaction",
-        items: items.map(({ sku: id, name, price, categoryTree: [department = "", category = "", subcategory = ""], brand }) => ({
-          transactionId,
-          id,
-          name,
-          price: price.toFixed(2),
-          department,
-          category,
-          subcategory,
-          brand
-        }))
-      };
-      Btg360.add(BTG360TransactionEvent);
-      break;
+        event: 'transaction',
+        items: items.map(
+          ({
+            sku: id,
+            name,
+            price,
+            categoryTree: [department = '', category = '', subcategory = ''],
+            brand,
+          }) => ({
+            transactionId,
+            id,
+            name,
+            price: price.toFixed(2),
+            department,
+            category,
+            subcategory,
+            brand,
+          })
+        ),
+      }
+      Btg360.add(BTG360TransactionEvent)
+      break
     }
-    case "vtex:productView": {
+    case 'vtex:productView': {
       const {
         product: {
           productId: id,
@@ -56,15 +64,19 @@ function handleMessages(e: PixelMessage) {
             {
               sellers: [
                 {
-                  commertialOffer: { Price: price }
-                }
-              ]
-            }
-          ]
-        }
-      } = e.data;
-      const [categoryTree] = categories;
-      const [department = "", category = "", subcategory = ""] = categoryTree.split("/").filter(item => item);
+                  commertialOffer: { Price: price },
+                },
+              ],
+            },
+          ],
+        },
+      } = e.data
+      const [categoryTree] = categories
+      const [
+        department = '',
+        category = '',
+        subcategory = '',
+      ] = categoryTree.split('/').filter(item => item)
       const BTG360ProductEventItem = {
         id,
         name,
@@ -72,42 +84,48 @@ function handleMessages(e: PixelMessage) {
         department,
         category,
         subcategory,
-        brand
-      };
+        brand,
+      }
       const BTG360ProductEvent = {
         account,
         domain,
-        event: "product",
-        items: [BTG360ProductEventItem]
-      };
-      Btg360.add(BTG360ProductEvent);
-      break;
+        event: 'product',
+        items: [BTG360ProductEventItem],
+      }
+      Btg360.add(BTG360ProductEvent)
+      break
     }
-    case "vtex:addToCart": {
-      const { items } = e.data;
+    case 'vtex:addToCart': {
+      const { items } = e.data
       const BTG360CartEvent = {
         account,
         domain,
-        event: "cart",
-        items: items.map(({ name, price, skuId: id, brand, category: categoryTree }) => {
-          const [department = "", category = "", subcategory = ""] = categoryTree.split("/");
-          return {
-            id,
-            name: name.toUpperCase(),
-            price: price.toFixed(2),
-            department,
-            category,
-            subcategory,
-            brand
-          };
-        })
-      };
-      Btg360.add(BTG360CartEvent);
-      break;
+        event: 'cart',
+        items: items.map(
+          ({ name, price, skuId: id, brand, category: categoryTree }) => {
+            const [
+              department = '',
+              category = '',
+              subcategory = '',
+            ] = categoryTree.split('/')
+            return {
+              id,
+              name: name.toUpperCase(),
+              price: price.toFixed(2),
+              department,
+              category,
+              subcategory,
+              brand,
+            }
+          }
+        ),
+      }
+      Btg360.add(BTG360CartEvent)
+      break
     }
   }
 }
 
 if (canUseDOM) {
-  window.addEventListener("message", handleMessages);
+  window.addEventListener('message', handleMessages)
 }
