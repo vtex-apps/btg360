22a1baa233f5fb43fe9f66a90491e81b7ea73891
Julio Moreira <hellojuliomoreira@gmail.com>
2019-09-26 15:50:54 -0300

    Update react/helpers/index.tsx
    Co-Authored-By: saviofelixmuniz <savio.muniz@ccc.ufcg.edu.br>

--- react/helpers/index.tsx
+++ react/helpers/index.tsx
@@ -2,6 +2,6 @@ export async function fetchEmail(): Promise<string> {
   const email = await fetch('/no-cache/profileSystem/getProfile')
     .then<ProfileData>(res => res.json())
     .then<string>(({ Email }) => Email)
-    .catch(_ => console.log(`Could'nt fetch logged in use email`))
+    .catch(_ => console.log(`Couldn't fetch logged in use email`))
   return email || ''
 }
